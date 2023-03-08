859649ee7cc4050fba4f19adc8327afcf98d660f
Wei Chern <lweichern@gmail.com>
2023-03-08 21:35:19 +0800

    Disable cursor on mobile and change styling of position section

--- components/home/Positions.jsx
+++ components/home/Positions.jsx
@@ -4,13 +4,13 @@ import Marquee from "react-fast-marquee";
 export default function Positions() {
   return (
     <section
-      className="flex text-white gap-4 text-5xl lg:text-8xl font-bold  bg-slate-600"
+      className="flex text-white gap-4 text-6xl lg:text-8xl font-bold  bg-slate-600"
       style={{ textShadow: "0 0 5px black" }}
     >
       <Marquee
         speed={70}
         gradientColor={[100, 116, 139]}
-        className="overflow-hidden pb-3"
+        className="overflow-hidden lg:pb-3 pb-2"
       >
         <div className="mx-3 md:mx-4 lg:mx-5">FRONTEND</div>
         <div className="mx-3 md:mx-4 lg:mx-5">BACKEND</div>
