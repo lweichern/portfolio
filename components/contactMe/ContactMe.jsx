d97eecb63e8be0779557a96388d23407de253b53
Wei Chern <lweichern@gmail.com>
2023-03-29 20:56:05 +0800

    Apply animation to contact me

--- components/contactMe/ContactMe.jsx
+++ components/contactMe/ContactMe.jsx
@@ -1,14 +1,20 @@
-import Image from "next/image";
 import Link from "next/link";
 import React from "react";
-import Divider from "../common/Divider";
+import { motion } from "framer-motion";
 
 export default function ContactMe() {
   return (
-    <Link href="mailto:lweichern@gmail.com">
-      <div className="fixed bottom-8 left-8 md:bottom-12 md:left-12 lg:left-16 bg-secondary text-primary text-xl w-24 h-24 font-semibold rounded-full flex justify-center items-center text-center opacity-80 neumorphism-shadow-contact-me cursor-pointer hover:bg-primary hover:text-secondary duration-150 animate-bounce-slow">
-        Contact Me
-      </div>
-    </Link>
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 1.5, delay: 5.3 }}
+      className="py-8 px-3 lg:w-[50%] text-center lg:text-left"
+    >
+      <Link href="mailto:lweichern@gmail.com">
+        <div className="fixed bottom-8 left-8 md:bottom-12 md:left-12 lg:left-16 bg-secondary text-primary text-xl w-24 h-24 font-semibold rounded-full flex justify-center items-center text-center opacity-80 neumorphism-shadow-contact-me cursor-pointer hover:bg-primary hover:text-secondary duration-150 animate-bounce-slow">
+          Contact Me
+        </div>
+      </Link>
+    </motion.div>
   );
 }
